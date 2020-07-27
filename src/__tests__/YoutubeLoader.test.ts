006f8abdacbb183a6d31019b8cbbdaf828f29517
Marius Butz <mbpictures13@gmail.com>
2020-07-27 17:07:12 +0200

    add tests for filtering

--- src/__tests__/YoutubeLoader.test.ts
+++ src/__tests__/YoutubeLoader.test.ts
@@ -1,4 +1,4 @@
-import { YoutubeLoader, VideoInfo } from '../YoutubeLoader';
+import { YoutubeLoader, VideoInfo, FilterType, StreamingInfo } from '../YoutubeLoader';
 
 test('video_id1', () => {
     let yt: YoutubeLoader = new YoutubeLoader('https://www.youtube.com/watch?v=jdskfjl_ajs');
@@ -28,4 +28,22 @@ test('video_links', () => {
 test('video_links_fail', () => {
     let yt: YoutubeLoader = new YoutubeLoader('https://www.youtube.com/watch?v=abcdefghijk');
     expect(yt.getVideoLinks()).rejects.toEqual(YoutubeLoader.VIDEO_NOT_FOUND_ERROR);
+});
+
+test('streaming_info_filter1', () => {
+    let yt: YoutubeLoader = new YoutubeLoader('https://www.youtube.com/watch?v=ahCwqrYpIuM');
+    yt.getFilteredStreamingInfo([[FilterType.EQUAL, "audio", false]]).then((infos: StreamingInfo[]) => {
+        infos.forEach((info: StreamingInfo) => {
+            expect(info.audio).toBe(false);
+        });
+    });
+});
+
+test('streaming_info_filter2', () => {
+    let yt: YoutubeLoader = new YoutubeLoader('https://www.youtube.com/watch?v=ahCwqrYpIuM');
+    yt.getFilteredStreamingInfo([[FilterType.GREATER_OR_EQUAL_THAN, "width", 720]]).then((infos: StreamingInfo[]) => {
+        infos.forEach((info: StreamingInfo) => {
+            expect(info.width).toBeGreaterThanOrEqual(720);
+        });
+    });
 });
